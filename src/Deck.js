1b4bfc8f23159952f0d7d988b5e230d4efe25996
Thibault Souef <souef.thibault@gmail.com>
2017-08-01 08:38:10 +0200

    wip

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Animated } fom 'react-native';
+
+class Deck extends Component {
+  render() {
+    return(
+      <View />
+    )
+  }
+}
+
+export default Deck;
